45f3c51b6f212e53724c99997db0bfa50beb1775
Joe Mulvaney <jmulvane@cshl.edu>
2016-03-14 17:04:53 -0400

    First thing: a simple proxy to Search.genes. Resolves #3

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const DEFAULT_PORT = 11011;
+
+var express = require('express')
+  , cors = require('cors')
+  , search = require('./src/search.js')
+  , app = express()
+  , port = process.env.PORT || DEFAULT_PORT;
+
+app.use(cors());
+
+app.get('/ensemblGenomes_gene', function(req, res, next){
+  console.log("Hitting ensemblGenomes_gene", req.query);
+  search(req.query).then(function(results) {
+    res.json(results.obj);
+  });
+});
+
+app.listen(11011, function(){
+  console.log('CORS-enabled web server listening on port ' + port);
+});
---
+++ src/search.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var grameneClient = require('gramene-search-client').client.grameneClient;
+var Q = require('q');
+
+function search(params) {
+  return grameneClient.then(function(client) {
+    var deferred, params;
+    deferred = Q.defer();
+    client['Search'].genes(params, deferred.resolve);
+    return deferred.promise;
+  });
+}
+
+module.exports = search;
