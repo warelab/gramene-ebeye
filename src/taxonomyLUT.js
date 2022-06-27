8051358f67b024ab1c8dd2c10b5336527e829a49
Andrew Olson <olson@cshl.edu>
2022-06-27 14:51:17 -0400

    also include display names from the maps collection

--- src/taxonomyLUT.js
+++ src/taxonomyLUT.js
@@ -7,6 +7,12 @@ var lut = {
 };
 
 grameneClient.then(function(client) {
+  client['Data access'].maps({rows: -1, fl: 'taxon_id,display_name'}, function(mapsResponse) {
+    mapsResponse.obj.forEach(m => {
+      lut.name2taxon_id[m.display_name] = m.taxon_id;
+      lut.taxon_id2name[m.taxon_id] = m.display_name;
+    });
+  });
   client['Data access'].taxonomy({subset:'gramene', rows: -1, fl: '_id,name'}, function(taxonResponse) {
     taxonResponse.obj.forEach(x => {
       lut.name2taxon_id[x.name] = x._id;
